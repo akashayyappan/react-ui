1b2064f42219a5ae7c78a3206cf132f78af89ce9
Akash Ayyappan(UST <210183@ust-global.com>
2022-03-02 10:39:56 +0530

    home component

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,8 +4,15 @@ import React from 'react';
 class Home extends React.Component {
     render(){
         return (
-            <div className="container">
+            <div className="container-home">
                 <input type="file" />
+                <br/><br/>
+                <div className="progress-bar">
+                    <div className="progress"></div>
+                </div>
+                <span>0% Completed</span>
+                <br/><br/><br/>
+                <button className="upload">View Dashboard</button>
             </div>
         );
     }
