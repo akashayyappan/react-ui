2cd410efe6ae3a55ca90b524898ea73461705b42
Meenakshi G <meenujanu27@gmail.com>
2022-03-22 17:46:24 +0530

    upload button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
-import React from 'react';
+import React, {useState} from 'react';
 import { Link, BrowserRouter, Routes, Route, Navigate, Outlet, useNavigate } from 'react-router-dom';
-
+import { Modal, Button } from 'antd';
 import Home from './components/home/Home';
 import Dashboard from './components/dashboard/Dashboard';
 import Recommendation from './components/recommendation/Recommendation';
 import Login from './components/login/Login';
 import Register from './components/register/Register';
-
-import { FaHome, FaChartPie, FaPhotoVideo, FaSignOutAlt } from 'react-icons/fa';
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
+import { FaHome, FaChartPie, FaPhotoVideo, FaSignOutAlt,FaFileUpload } from 'react-icons/fa';
+// import { Upload } from './components/upload/Upload';
 
 class App extends React.Component {
   constructor() {
@@ -16,9 +18,16 @@ class App extends React.Component {
     let auth = sessionStorage.getItem('auth');
     if(!auth) auth = false;
     this.state = {
-      isUserAuthenticated: auth
+      isUserAuthenticated: auth,
+      setIsModalVisible:false,
+      isModalVisible:false
     }
   }
+  // const [isModalVisible, setIsModalVisible]=useState(false);
+  // onUploadClick(){
+  //   name: "Upload";
+  //   this.setState({ showHideDemo1: false })
+  // }
 
   onLoginHandle = value => {
     this.setState({ isUserAuthenticated: value })
@@ -31,12 +40,24 @@ class App extends React.Component {
           {this.state.isUserAuthenticated &&
             <div>
               <header>
-                <h3>Analysis System</h3>
+                <h3>Viewer base Analysis</h3>
                 <div className="navbar">
                   <ul className="nav">
                     <li><Link to={'/home'} className="nav-item"><FaHome /><span>Home</span></Link></li>
                     <li><Link to={'/dashboard'} className="nav-item"><FaChartPie /><span>Dashboard</span></Link></li>
                     <li><Link to={'/rec'} className="nav-item"><FaPhotoVideo /><span>Recommendation</span></Link></li>
+                    <li>
+                      {/* <Link to={'/rec'} className="nav-item"><FaFileUpload /><span>Upload</span></Link> */}
+                    <FaFileUpload /><span className="nav-item" onClick={()=>this.setState({setIsModalVisible: !this.state.setIsModalVisible})}>
+                    Upload</span>
+                    <Modal title="Basic Modal" visible={this.state.setIsModalVisible} onOk={this.state.setIsModalVisible} onCancel={this.state.setIsModalVisible}>
+                    <p>Dataset</p><input type="file" />
+                    <p>Matrix</p><input type="file" />
+                    </Modal>
+                    </li>
+                    {/* <button className="nav-item"  onClick={()=>this.setState({setIsModalVisible: !this.state.setIsModalVisible})}>
+                    <FaFileUpload /><span>Upload</span>
+                    </button> */}
                   </ul>
                 </div>
                 <Link to={'/login'} className="logout"><FaSignOutAlt /><span>Logout</span></Link>
@@ -52,6 +73,7 @@ class App extends React.Component {
             <Route path='/register' element={<Register />} />
             <Route path='/dashboard' element={<Dashboard />} />
             <Route path='/rec' element={<Recommendation />} />
+            {/* <Route path='/upload' element={<Upload />} /> */}
           </Routes>
         </BrowserRouter>
         <Outlet />
---
+++ src/components/upload/Upload.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { FaUpload } from 'react-icons/fa';
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
+
+export function Upload (){
+    <Popup trigger={<button> Upload</button>} position="right center">
+      <div>Popup content here !!</div>
+    </Popup>
+};
